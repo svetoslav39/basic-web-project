36bdb3a197e705d7d9d79c59385cd3990e3cf73a
Svetoslav Stoyanov <svetoslav39@gmail.com>
2019-04-12 00:24:06 +0300

    add body-parser

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+var bodyParser = require('body-parser')
 const port = 3000
 const app = express();
 
@@ -9,15 +10,19 @@ app.engine('hbs', handlebars({
 }));
 app.set('view engine', 'hbs');
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render("home/index");
 });
-app.get('/articles/create',(req,res)=>{
+app.get('/articles/create', (req, res) => {
     res.render('articles/create')
 })
 app.get('/articles', (req, res) => {
     res.render('articles/index')
+})
+app.post('/articles/create', (req, res) => {
+    console.log(req.body)
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
